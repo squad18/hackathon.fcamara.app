e872ea7f5932c0a47eb0bf126d54f8e0c333979c
GersonMM <gmelao33@gmail.com>
2021-03-30 02:22:32 -0300

    Tela de doações criada, implementação http angular e consumo api mockada

--- appdoacao/src/app/tab3/tab3.module.ts
+++ appdoacao/src/app/tab3/tab3.module.ts
@@ -5,11 +5,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab3PageRoutingModule } from './tab3-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   imports: [
+    HttpClientModule,
     IonicModule,
     CommonModule,
     FormsModule,
--- appdoacao/src/app/tab3/tab3.page.ts
+++ appdoacao/src/app/tab3/tab3.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlunoModel } from '../models/aluno.model';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
 
-  constructor() {}
+export class Tab3Page implements OnInit {
 
+  listaAlunos: AlunoModel[];
+  headers: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders();
+    this.headers = this.headers.append('Content-Type', 'application/json')
+  }
+
+  ngOnInit(){
+    this.obterAlunos();
+  }
+
+  get(): Observable<string> {   
+    return this.http.get<string>("https://run.mocky.io/v3/c36ce3ad-87d1-4dea-a5bc-2d0375550a58", { headers: this.headers })
+    .pipe(take(1));
+  }
+
+  obterAlunos() {
+    this.get().subscribe((result: any) => {
+      this.listaAlunos = result.data;
+    })
+  }
 }
