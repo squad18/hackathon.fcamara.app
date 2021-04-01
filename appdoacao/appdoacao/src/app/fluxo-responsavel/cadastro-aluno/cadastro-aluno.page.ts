c01e623294ebc79d954d8dbff4a8f1c871e82a1a
friasrafael <rafaelfriasdacosta@gmail.com>
2021-04-01 01:25:18 -0300

    Início de desenvolvimento da versão final.
    Início do desenvolvimento do protótipo alta do Fluxo do Responsável. Mudança do tipo de documento de Tabs para Blank.

---
+++ appdoacao/appdoacao/src/app/fluxo-responsavel/cadastro-aluno/cadastro-aluno.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-aluno',
+  templateUrl: './cadastro-aluno.page.html',
+  styleUrls: ['./cadastro-aluno.page.scss'],
+})
+export class CadastroAlunoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
